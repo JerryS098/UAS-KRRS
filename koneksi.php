70a94d51111ec8c2fbb693a1d72279c0210ef1fa
Jerry Setiawan - 535210005 <111833928+JerryS098@users.noreply.github.com>
2022-12-05 06:59:00 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,19 @@
+<?php
+$koneksi = mysqli_connect("localhost", "root", "", "krrs");
+
+
+/*
+
+Untuk mengecek apakah database terhubung atau tidak
+
+if ($koneksi) {
+    $sukses = mysqli_select_db($koneksi , "krrs");
+    echo "Database Terhubung";
+    if (!$sukses) {
+        echo "Database Tidak Terhubung";
+    }
+} else {
+    echo "Mysqli tidak terhubung";
+}
+*/
+?>
